41da9c95cd6b2fb3621130b6659b3ef012c9d132
Mohammed Faisal <faisalshahna38@gmail.com>
2024-07-22 23:42:49 +0530

    repnsv and footer

--- main/src/components/Footer.jsx
+++ main/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-950 text-white p-3'>
-      Footer
+    <div className='bg-gray-950 text-white p-3 flex justify-center items-center'>
+      <p>Copyright &copy; 2024 mdfaisalshahna</p>
     </div>
   )
 }
--- main/src/components/Hero.jsx
+++ main/src/components/Hero.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import hero from '../assets/hero.jpg'
 const Hero = () => {
   return (
-    <div className='flex bg-gray-900 h-[95vh]'>
-      <div className='flex flex-col items-start justify-center text-gray-500 w-2/3 p-24 gap-5'>
-        <h2 className='text-4xl'>Welcome to my portfolio, I am</h2>
-        <h1 className='text-7xl text-gray-100'>Mohammed Faisal</h1>
-        <h2 className='text-2xl'>Web Developer | Web Designer | Photographer | Video Editor</h2>
+    <div className='flex flex-col-reverse justify-center items-center bg-gray-900 h-[95vh] lg:flex-row'>
+      <div className='flex flex-col items-start justify-center text-gray-500 p-24 gap-5 lg:w-2/3'>
+        <h2 className='text-2xl lg:text-4xl'>Welcome to my portfolio, I am</h2>
+        <h1 className='text-4xl lg:text-6xl 2xl:text-8xl text-gray-100'>Mohammed Faisal</h1>
+        <h2 className='text-lg lg:text-2xl'>Web Developer | Web Designer | Photographer | Video Editor</h2>
       </div>
-      <div className='flex justify-center items-center'>
-        <img className='h-[60%] rounded-3xl' src={hero} alt=''/>
+      <div className='flex justify-center items-center h-[50%]'>
+        <img className='h-[100%] rounded-3xl' src={hero} alt=''/>
       </div>
     </div>
   )
